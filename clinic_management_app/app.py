1e137d719106b3c8164ccd7fb53d23b111e204ae
vanchuongminhthuan-maker <vanchuong.minhthuan@gmail.com>
2025-09-01 19:31:30 +0700

    Update app.py

--- clinic_management_app/app.py
+++ clinic_management_app/app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clinic.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+@app.before_first_request
+def init_db():
+    db.create_all()
 # ==========================
 # Database Models
 # ==========================
